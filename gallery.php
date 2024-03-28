79c9f1be5f46c4dc0eca34e0854f5768f65c6a97
Polsing Ambule <97845074+polsingambule435@users.noreply.github.com>
2024-03-28 21:48:49 +0530

    gallery file
    gallery file

---
+++ gallery.php
@@ -0,0 +1,133 @@
+<?php
+   include 'header.php';
+?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"/>
+        <!-- CONTENT START -->
+        <div class="page-content">
+        
+            <!-- INNER PAGE BANNER -->
+            <div class="sx-bnr-inr overlay-wraper bg-parallax bg-top-center"  data-stellar-background-ratio="0.5"  style="background-image:url(assets/images/banner/cmn-img.jpg);">
+                <div class="overlay-main bg-black opacity-07"></div>
+                <div class="container">
+                    <div class="sx-bnr-inr-entry">
+                        <div class="banner-title-outer">
+                            <div class="banner-title-name">
+                                <h2 class="m-tb0">Gallery</h2>                                
+                                <p>Step into Vardhaman Group's world – where tea, franchises, and delicious moments come to life in a snapshot of quality and opportunities!</p>
+                            </div>
+                        </div>
+                        <!-- BREADCRUMB ROW -->                            
+                        
+                            <div>
+                                <ul class="sx-breadcrumb breadcrumb-style-2">
+                                    <li><a href="javascript:void(0);">Home</a></li>
+                                    <li>Gallery</li>
+                                </ul>
+                            </div>
+                        
+                        <!-- BREADCRUMB ROW END -->                        
+                    </div>
+                </div>
+            </div>
+            <!-- INNER PAGE BANNER END -->
+             
+            <!-- ABOUT COMPANY START -->
+            <div class="section-full mobile-page-padding p-t80 p-b80 bg-gray">
+                <div class="container">
+              <article class="row row-cols-1 row-cols-sm-2 g-2 d-flex justify-content-center">
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g1.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g1.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g2.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g2.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g3.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g3.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g4.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g4.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g5.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g5.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g6.jpeg" data-caption="Cute Puppy">
+                    <img src="assets/images/gallery/g6.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g7.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g7.jpeg" />
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-3 mb-4 text-center">
+                  <a data-fancybox="gallery" data-src="assets/images/gallery/g8.jpeg" data-caption="">
+                    <img src="assets/images/gallery/g8.jpeg" />
+                  </a>
+                </div>
+              </article>
+            </div>
+            <div class="container" id="videos">
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/6Kpi4eTfpiE?si=9xsS8LBZD1_WxqdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/iHev5Fvd8dY?si=RIeXQ1BpFVRrgw14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/QBljCvn3c5Q?si=pYcHGtezmKKsi_nv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/eDL5bjwYvYU?si=5AqXGl0P8EavSZnb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/cHNh3lACHpA?si=BHAX23P_ToULMML-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-md-4">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/3WTltpWxFeY?si=N8zteZQmDfa46Ub7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+            </div>   
+            <!-- ABOUT COMPANY END -->
+            
+                         
+            
+        </div>
+    <script type="text/javascript">
+            const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
+                preload: 1
+                });
+
+                Fancybox.assign('[data-fancybox="carousel-gallery"]', {
+                closeButton: "top",
+                Thumbs: false,
+                Carousel: {
+                Dots: true,
+                on: {
+                change: (that) => {
+                myCarousel.slideTo(myCarousel.getPageforSlide(that.page), {
+                friction: 0
+                });
+                }
+                }
+                }
+                });
+
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
+        <script src="assets/other-assets/vendors/jquery/jquery-3.6.0.min.js"></script>    <!-- CONTENT END -->
+<?php
+   include 'footer.php';
+?>
